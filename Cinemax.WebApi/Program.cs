905ad98d4330d9a658f7c54c92392d3adfbe8294
Jerson97 <jersonramirezs97@gmail.com>
2025-11-11 20:31:10 -0500

    Feat: se agrego cors y token en el swagger

--- Cinemax.WebApi/Program.cs
+++ Cinemax.WebApi/Program.cs
@@ -2,6 +2,7 @@ using Cinemax.Application;
 using Cinemax.Persistence;
 using Cinemax.WebApi;
 using Cinemax.WebApi.Middleware.ErrorMiddlewares;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,7 +25,54 @@ builder.Services.AddIdentityAndJwt(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "API Ticket", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "Bearer Authentication with JWT Token",
+        Type = SecuritySchemeType.Http
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+});
+
+#region CORS
+
+
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("CorsPolicy",
+          builder => builder
+              .WithOrigins(new string[] {
+                "http://localhost:3000" ,
+                "http://localhost:3001" ,
+                "http://localhost:5173" ,
+                "http://localhost:5174"
+              })
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials());
+});
+
+#endregion
 
 var app = builder.Build();
 
@@ -37,6 +85,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseMiddleware<ErrorHandlerMiddleware>();
@@ -47,4 +97,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run(); 
